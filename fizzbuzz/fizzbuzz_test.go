2af3aec4a90ce6933016f3ccad2e5e99dc114c74
Jennifer Strejevitch <jstrejevitch@vmware.com>
2021-07-21 12:18:00 +0100

    test suite setup

---
+++ fizzbuzz/fizzbuzz_test.go
@@ -0,0 +1,13 @@
+package fizzbuzz_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Fizzbuzz", func() {
+
+	It("fails", func() {
+		Expect(true).To(Equal(true))
+	})
+})
